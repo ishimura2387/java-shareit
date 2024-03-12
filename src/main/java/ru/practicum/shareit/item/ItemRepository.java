30bd18043d7e5d3f58bdd34b05c9e510a4cc6be6
Shestakov_Denis <shestakov_denis_job@mail.ru>
2024-03-12 20:49:34 +0800

    sprint 14 v3.2

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -11,6 +11,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query(" select i from Item i where upper(i.name) like upper(concat('%', ?1, '%')) or upper(i.description) " +
             "like upper(concat('%', ?1, '%')) and i.available = true")
     List<Item> getItemsForRent(String text);
-    
+
     Item findByIdAndOwnerId(long itemId, long ownerId);
 }
